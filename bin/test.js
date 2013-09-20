07a3827cb4940a1d7618f31f333f2555c97c6635
Johan Jatko <johan@pie-studios.com>
2013-09-20 08:32:09 +0200

    Travis test

--- bin/test.js
+++ bin/test.js
@@ -1,12 +1,12 @@
-var rcon = require('./lib/rcon-proxy');
+var rcon = require('../lib/rcon-proxy');
 var options = {
 	listenHost: "127.0.0.1", // What host should proxy listen on?
 	listenPort: "28962", // What port should proxy listen on?
 	proxyHost: "127.0.0.1", // What host should it proxy to? (server's net_ip)
-	proxyPort: 28961, // What port should it proxy to? (server's net_port)
+	proxyPort: 28960, // What port should it proxy to? (server's net_port)
 	bindHost: "127.0.0.1", // What host should proxy clients connect from?
 	serverPassword: "pass", // The RCon password the server has
-	authLib: new require('./lib/simple-auth')()
+	authLib: new require('../lib/simple-auth')()
 }
 
 var srv = rcon.createServer(options);
