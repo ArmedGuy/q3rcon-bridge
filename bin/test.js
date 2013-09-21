ed42b8a815a39a602126d576163333e8ad352e78
Johan Jatko <johan@pie-studios.com>
2013-09-21 22:09:10 +0200

    New version with readme

--- bin/test.js
+++ bin/test.js
@@ -6,7 +6,7 @@ var options = {
 	proxyPort: 28960, // What port should it proxy to? (server's net_port)
 	bindHost: "127.0.0.1", // What host should proxy clients connect from?
 	serverPassword: "pass", // The RCon password the server has
-	authLib: new require('../lib/simple-auth')(process.cwd() + "/bin/auth.txt")
+	authLib: new require('../auth/simple')(process.cwd() + "/bin/auth.txt")
 }
 
 var srv = rcon.createServer(options);
