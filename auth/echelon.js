ed89c11738d5bf9332a624ce9664a5da88eb3859
Johan Jatko <johan@pie-studios.com>
2013-09-25 11:28:00 +0200

    Fixed Echelon module mysql usage

--- auth/echelon.js
+++ auth/echelon.js
@@ -3,21 +3,22 @@ function EchelonAuthLib(options) {
 	authl = this;
 	this.users = new Array();
 	this.options = options;
+	this.fetchUsers();
+}
+EchelonAuthLib.prototype.fetchUsers = function() {
+	this.users = [];
 	this.conn = mysql.createConnection({
 		host: options.host,
 		user: options.user,
 		password: options.password
 	});
 	this.conn.connect();
-	this.fetchUsers();
-}
-EchelonAuthLib.prototype.fetchUsers = function() {
-	this.users = [];
 	this.conn.query("SELECT * FROM `" + this.options.database + "`.`users`", function(err, rows) {
 		for(var i in rows) {
 			authl.users.push({username: rows[i].username, password: rows[i].password});
 		}
 	});
+	this.conn.end();
 }
 EchelonAuthLib.prototype.auth = function(username, password, callback) {
 	password = require('crypto').createHash('md5').update(password).digest("hex")
